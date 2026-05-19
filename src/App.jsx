47ab7d68f2708f8dacfc018b3573eecf9ff9c330
Niroj9719 <basnetniroj64@gmail.com>
2026-05-19 19:59:01 +0545

    Resolve leftover merge markers in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,9 @@ import StaffManagement from "./pages/Admin/ManageStaff";
 import FinancialReports from "./pages/Admin/FinancialReports";
 import LowStockAlerts from "./pages/Admin/LowStockAlerts";
 import Notifications from "./pages/Admin/Notifications";
-<<<<<<< HEAD
 import PendingCreditReminders from "./pages/Admin/PendingCreditReminders";
 
 // Customer Pages
-=======
-import CustomerLayout from "./layouts/CustomerLayout";
->>>>>>> 71052fa15d25b50068e3316a32f4943c090e430e
 import CustomerSignup from "./pages/customer/CustomerSignup";
 import CustomerLogin from "./pages/customer/CustomerLogin";
 import CustomerProfile from "./pages/customer/CustomerProfile";
@@ -309,36 +305,4 @@ const styles = {
 };
 
 export default App;
-=======
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Customer auth (Feature 12) */}
-        <Route path="/login" element={<CustomerLogin />} />
-        <Route path="/signup" element={<CustomerSignup />} />
 
-        {/* Customer portal (Features 12, 13, 14, 16) */}
-        <Route element={<CustomerLayout />}>
-          <Route path="/profile" element={<CustomerProfile />} />
-          <Route path="/services" element={<CustomerServices />} />
-          <Route path="/purchase" element={<CustomerPurchase />} />
-          <Route path="/history" element={<CustomerHistory />} />
-        </Route>
-
-        {/* Admin portal */}
-        <Route path="/" element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="staff-management" element={<StaffManagement />} />
-          <Route path="financial-reports" element={<FinancialReports />} />
-          <Route path="low-stock-alerts" element={<LowStockAlerts />} />
-          <Route path="notifications" element={<Notifications />} />
-          <Route path="pending-credit-reminders" element={<PendingCreditReminders />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
->>>>>>> 71052fa15d25b50068e3316a32f4943c090e430e
